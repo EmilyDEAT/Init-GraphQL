b030e1fd320b7264d4b05d8ecba052edd969a9ea
Emily Déat <emily.deat@gmail.com>
2020-11-12 10:38:23 +0100

    Init project with graphQL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { useQuery, gql } from '@apollo/client';
+
+const EXCHANGE_RATES = gql`
+  query {
+    launches(limit: 5) {
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
+
+const App = () => {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+if (error) return <p>Error !</p>;
+
+  return data.launches.map(({ launch_date_utc, launch_success, rocket, links, details }, i) => (
+    <div key={i}>
+      <h2>{rocket.rocket_name}</h2>
+      <p>
+        Lauche date : {launch_date_utc}
+      </p>
+      <p>
+        Success: {launch_success ? 'true' : 'false'}
+      </p>
+      <p>
+        Video: <a href={links.video_link}>here</a>
+      </p>
+      <p>
+        Details: {details}
+      </p>
     </div>
-  );
+  ));
 }
 
 export default App;
